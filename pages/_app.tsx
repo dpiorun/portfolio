a4302d08493cd4dfbae0b10763ed75dbf8752f51
Damian Piorun <damian.piorun@gmail.com>
2023-05-05 10:20:10 +0200

    feat: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
 
 import { Mulish, Montserrat } from "next/font/google";
 
@@ -44,6 +45,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <div className={`${mulish.variable} ${montserrat.variable}`}>
         <AnimatePresence mode="wait" initial={false}>
           <Component {...pageProps} />
+          <Analytics />
         </AnimatePresence>
       </div>
     </>
